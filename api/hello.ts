94177e267ca52bdd66b32b07b3e1ba76a1523959
kingvyas <kumarvyasss@gmail.com>
2022-09-12 08:59:19 +0530

    replace vercel with cloudflare

--- api/hello.ts
+++
@@ -1,7 +0,0 @@
-import { VercelRequest, VercelResponse } from '@vercel/node';
-
-export default (req: VercelRequest, res: VercelResponse) => {
-  return res.json({
-    name: `Hello, from ${req.url} I'm now an Edge Function!`,
-  });
-};
