c1dacd469cff3999cc870b5729baa7ecd29613cc
kingvyas <kumarvyasss@gmail.com>
2022-09-12 09:44:41 +0530

    made bot worked

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,21 @@
-export interface Env {}
+interface IPayload {
+  message?: { chat: { id: string }; text: string };
+}
 
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response("Hello World!");
+  async fetch(req: Request) {
+    if (req.method === "POST") {
+      
+      const payload: IPayload = await req.json();
+      if (payload.message) {
+        const bot = "5681432295:AAFlKNc0IpI4JfTPumIpUL5tgk2GSpr6rDU";
+        const chatId = payload.message.chat.id;
+        const text = payload.message.text + " over";
+        const url = `https://api.telegram.org/bot${bot}/sendMessage?chat_id=${chatId}&text=${text}`;
+
+        await fetch(url).then((resp) => resp.json());
+      }
+    }
+    return new Response("OK");
   },
 };
