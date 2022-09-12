fb3a8471286d5c5fbf0af8781e146f88b3a0343c
kingvyas <kumarvyasss@gmail.com>
2022-09-12 10:02:58 +0530

    fixed typescript issue and removed unnecessary files

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,24 @@
+import { Message } from "../telegram";
+
 interface IPayload {
-  message?: { chat: { id: string }; text: string };
+  message?: Message;
 }
 
 export default {
   async fetch(req: Request) {
     if (req.method === "POST") {
-      
+
       const payload: IPayload = await req.json();
+
       if (payload.message) {
+
         const bot = "5681432295:AAFlKNc0IpI4JfTPumIpUL5tgk2GSpr6rDU";
         const chatId = payload.message.chat.id;
-        const text = payload.message.text + " over";
+        const name = payload.message.chat.first_name
+        const text = payload.message.text
+
+        const msg = `Hello ${name}, \nMessage: ${text} \n\nHave a great day 😎`;
+
         const url = `https://api.telegram.org/bot${bot}/sendMessage?chat_id=${chatId}&text=${text}`;
 
         await fetch(url).then((resp) => resp.json());
