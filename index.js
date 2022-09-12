3cc0bd1f22cc961fca0f9ec7e56c9a6bad63c6cd
kingvyas <kumarvyasss@gmail.com>
2022-09-12 22:39:44 +0530

    fixed minor issue in code

--- index.js
+++ index.js
@@ -6,8 +6,12 @@ const token = "5681432295:AAFlKNc0IpI4JfTPumIpUL5tgk2GSpr6rDU";
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", async (msg) => {
+
   const chatId = msg.chat.id;
   const text = msg.text;
-  const secret = (await saveSecret({ text })).data.id;
+
+  const res = await saveSecret({ text });
+  const secret = res.data.id;
+
   bot.sendMessage(chatId, secret);
 });
