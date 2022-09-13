aefac5082acb89fc3df3c6a7a2faebb73509608a
kingvyas <kumarvyasss@gmail.com>
2022-09-13 11:28:06 +0530

    made code look better

--- index.js
+++ index.js
@@ -7,39 +7,41 @@ const token = "5681432295:AAFlKNc0IpI4JfTPumIpUL5tgk2GSpr6rDU";
 const bot = new TelegramBot(token, { polling: true });
 
 bot.on("message", async (msg) => {
+  // Chat id and text sent by user
   const chatId = msg.chat.id;
   const text = msg.text;
 
+  // Reply text to user to choose one option
   const reply = "*Recived your text* \n\nChoose one of the options:";
 
-  bot.sendMessage(chatId, reply, {
-    disable_web_page_preview: true,
-    parse_mode: "Markdown",
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: "Text", callback_data: "text" },
-          { text: "Redirect", callback_data: "redirect" },
-        ],
-        [{ text: "Decrypt it", callback_data: "decrypt" }],
-      ],
-    },
-  });
+  // Sending reply with inline options for what to do
+  bot.sendMessage(chatId, reply, getReplyOptions());
 
+  // Listening for user callback of option he clicked
   bot.on("callback_query", async (query) => {
-    const q = query.data;
+    // Query data type (which is the option he chose) and message id of query
+    const type = query.data;
     const messageId = query.message.message_id;
 
+    // Answering the query according to query option he chose
     bot.answerCallbackQuery(query.id);
-    if (q === "text") {
+
+    // If the option is 'text'
+    if (type === "text") {
+      // Getting document and its id to save data
       const doc = getDocRef();
       const docId = doc.id;
+
+      // Generating a random key to encrypt
       const key = genKey();
 
-      const reply = `Your secret link:\nst.hrvs.me/${docId}#${key}`;
+      // Link of the secret
+      const link = `Your secret link:\nhttps://st.hrvs.me/${docId}#${key}`;
 
-      bot.editMessageText(reply, { chat_id: chatId, message_id: messageId });
+      // Editing the original message to avoid clutter and duplicate clicking
+      bot.editMessageText(link, { chat_id: chatId, message_id: messageId });
 
+      // Saving the secret to database
       await saveSecret(getData(text, key, q), doc);
     } else {
       bot.editMessageText("It is under construction", {
@@ -61,3 +63,19 @@ const getData = (secret, key, type) => {
 };
 
 const getHash = (url) => url.split("#")[1];
+
+const getReplyOptions = (arr) => {
+  return {
+    disable_web_page_preview: true,
+    parse_mode: "Markdown",
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "Text", callback_data: "text" },
+          { text: "Redirect", callback_data: "redirect" },
+        ],
+        [{ text: "Decrypt it", callback_data: "decrypt" }],
+      ],
+    },
+  };
+};
