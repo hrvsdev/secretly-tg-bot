d7072dfa4b6762a1058c19808ab30c9d16acf4fe
kingvyas <kumarvyasss@gmail.com>
2022-09-13 10:45:32 +0530

    fixed all the bugs in inline keyboard and made it worked

--- index.js
+++ index.js
@@ -10,13 +10,7 @@ bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
 
-  const doc = getDocRef();
-  const docId = doc.id;
-  const key = genKey();
-
-  const reply = "*Recived your text* \n\nChoose one of the options:"
-
-  const link = `st.hrvs.me/${docId}#${key}`;
+  const reply = "*Recived your text* \n\nChoose one of the options:";
 
   bot.sendMessage(chatId, reply, {
     disable_web_page_preview: true,
@@ -27,22 +21,34 @@ bot.on("message", async (msg) => {
           { text: "Text", callback_data: "text" },
           { text: "Redirect", callback_data: "redirect" },
         ],
-        [{ text: "Decrypt it", url: "decrypt" }],
+        [{ text: "Decrypt it", callback_data: "decrypt" }],
       ],
     },
   });
 
-  bot.on("callback_query", (query)=>{
-    const q = query.data
-    
-    const data = getData(text, key, "text");
-    
-    // await saveSecret(data, doc);
-    
-    // bot.sendMessage(chatId, "Saved !");
-  })
-});
+  bot.on("callback_query", async (query) => {
+    const q = query.data;
+    const messageId = query.message.message_id;
 
+    bot.answerCallbackQuery(query.id);
+    if (q === "text") {
+      const doc = getDocRef();
+      const docId = doc.id;
+      const key = genKey();
+
+      const reply = `Your secret link:\nst.hrvs.me/${docId}#${key}`;
+
+      bot.editMessageText(reply, { chat_id: chatId, message_id: messageId });
+
+      await saveSecret(getData(text, key, q), doc);
+    } else {
+      bot.editMessageText("It is under construction", {
+        chat_id: chatId,
+        message_id: messageId,
+      });
+    }
+  });
+});
 
 const getData = (secret, key, type) => {
   const data = {
