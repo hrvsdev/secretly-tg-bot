d5cb925b90cff58833602abcfe848107609197cb
kingvyas <kumarvyasss@gmail.com>
2022-09-13 10:11:24 +0530

    using inline keyboard for better usage

--- index.js
+++ index.js
@@ -6,34 +6,47 @@ const token = "5681432295:AAFlKNc0IpI4JfTPumIpUL5tgk2GSpr6rDU";
 
 const bot = new TelegramBot(token, { polling: true });
 
-bot.onText(/\/start/, (msg) => {
-  bot.sendMessage(msg.chat.id, "Welcome");
-});
-
 bot.on("message", async (msg) => {
   const chatId = msg.chat.id;
   const text = msg.text;
-
-  console.log(text)
 
   const doc = getDocRef();
   const docId = doc.id;
   const key = genKey();
 
+  const reply = "*Recived your text* \n\nChoose one of the options:"
+
   const link = `st.hrvs.me/${docId}#${key}`;
 
-  bot.sendMessage(chatId, link, { disable_web_page_preview: true });
+  bot.sendMessage(chatId, reply, {
+    disable_web_page_preview: true,
+    parse_mode: "Markdown",
+    reply_markup: {
+      inline_keyboard: [
+        [
+          { text: "Text", callback_data: "text" },
+          { text: "Redirect", callback_data: "redirect" },
+        ],
+        [{ text: "Decrypt it", url: "decrypt" }],
+      ],
+    },
+  });
 
-  const data = getData(text, key);
-
-  await saveSecret(data, doc);
-
-  bot.sendMessage(chatId, "Saved !");
+  bot.on("callback_query", (query)=>{
+    const q = query.data
+    
+    const data = getData(text, key, "text");
+    
+    // await saveSecret(data, doc);
+    
+    // bot.sendMessage(chatId, "Saved !");
+  })
 });
 
-const getData = (secret, key) => {
+
+const getData = (secret, key, type) => {
   const data = {
-    type: "text",
+    type: type,
     secret: secret,
     isEncryptedWithPassword: false,
     readReceiptEmail: "",
