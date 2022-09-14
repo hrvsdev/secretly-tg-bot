78adf4cdd2041b6540ff6e4ee658ec7de7285ba7
kingvyas <kumarvyasss@gmail.com>
2022-09-14 11:36:30 +0530

    showing keybaord only if text is url

--- index.js
+++ index.js
@@ -1,23 +1,38 @@
 const TelegramBot = require("node-telegram-bot-api");
-const { saveSecret, getDocRef, getSecret } = require("./firebase/db");
-const { encrypt, genKey, getIdandHash, decrypt } = require("./utils");
+const isUrl = require("is-url");
+const prependHttp = require("prepend-http");
+
+const { saveSecret, getDocRef } = require("./firebase/db");
+const { encrypt, genKey } = require("./utils");
 
 const token = "5681432295:AAFlKNc0IpI4JfTPumIpUL5tgk2GSpr6rDU";
 
 const bot = new TelegramBot(token, { polling: true });
 
-let text
+let text;
 
 bot.on("message", async (msg) => {
   // Chat id and text sent by user
   const chatId = msg.chat.id;
   text = msg.text;
 
-  // Reply text to user to choose one option
-  const reply = `*Recived your text:* \n${text} \n\nChoose one of the options:`;
+  // Checking if text is a URL
+  const prependedText = prependHttp(text);
+  const isTextURL = isUrl(prependHttp);
+
+  // Reply to user after receiving text
+  let reply;
+
+  // Showing options only if text is URL
+  if (isTextURL) {
+    reply = "*Recived your text 📥:* \nChoose one of the options :"
+  }
+  else {
+    reply = "Sending you your secret 📩"
+  }
 
   // Sending reply with inline options for what to do
-  bot.sendMessage(chatId, reply, getReplyOptions(text));
+  bot.sendMessage(chatId, reply, getReplyOptions(isTextURL));
 });
 
 // Listening for user callback of option he clicked
@@ -26,30 +41,29 @@ bot.on("callback_query", async (query) => {
   const chatId = query.from.id;
   const type = query.data;
   const msgId = query.message.message_id;
-  
+
   // Answering the query according to query option he chose
   await bot.answerCallbackQuery(query.id);
-  
-  // If the option is 'text' or 'redirect'
-  if (type === "text" || type === "redirect") {
-    // Getting document and its id to save data
-    const doc = getDocRef();
-    const docId = doc.id;
-
-    // Generating a random key to encrypt
-    const key = genKey();
-
-    // Link of the secret
-    const link = `Your secret link:\nhttps://st.hrvs.me/${docId}#${key}`;
-
-    // Editing the original message to avoid clutter and duplicate clicking
-    bot.editMessageText(link, getEditMsgOptions(chatId, msgId));
-
-    // Saving the secret to database
-    await saveSecret(getData(text, key, type), doc);
-    bot.sendMessage(chatId, "Saved! 👍🏼");
-  }
 });
+
+const sendMessage = async (text, type, bot, chatId, msgId) => {
+  // Getting document and its id to save data
+  const doc = getDocRef();
+  const docId = doc.id;
+
+  // Generating a random key to encrypt
+  const key = genKey();
+
+  // Link of the secret
+  const link = `Your one-time secret link:\nhttps://st.hrvs.me/${docId}#${key}`;
+
+  // Editing the original message to avoid clutter and duplicate clicking
+  bot.editMessageText(link, getEditMsgOptions(chatId, msgId));
+
+  // Saving the secret to database
+  await saveSecret(getData(text, key, type), doc);
+  bot.sendMessage(chatId, "Saved! 👍🏼");
+};
 
 const getEditMsgOptions = (chatId, messageId) => {
   return {
@@ -70,18 +84,15 @@ const getData = (secret, key, type = "text") => {
   return encrypt(data, key);
 };
 
-const getReplyOptions = (msg) => {
-  return {
-    disable_web_page_preview: true,
-    parse_mode: "Markdown",
-    reply_markup: {
-      inline_keyboard: [
-        [
-          { text: "Text", callback_data: "text" },
-          { text: "Redirect", callback_data: "redirect" },
-        ],
-        [{ text: "Decrypt it", callback_data: "decrypt" }],
-      ],
-    },
-  };
+const getReplyOptions = (isTextURL) => {
+  const parse_mode = "Markdown"
+  // const reply_markup = {
+  //   inline_keyboard: [
+  //     [
+  //       { text: "Text", callback_data: "text" },
+  //       { text: "Redirect", callback_data: "redirect" },
+  //     ],
+  //   ],
+  // },
+
 };
