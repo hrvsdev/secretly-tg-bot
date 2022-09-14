43de58757255dd0224dcd007b72f33a9d2d82a8a
kingvyas <kumarvyasss@gmail.com>
2022-09-14 09:41:08 +0530

    completed the bot with basic functionality

--- index.js
+++ index.js
@@ -6,80 +6,83 @@ const token = "5681432295:AAFlKNc0IpI4JfTPumIpUL5tgk2GSpr6rDU";
 
 const bot = new TelegramBot(token, { polling: true });
 
+let text
+
 bot.on("message", async (msg) => {
   // Chat id and text sent by user
   const chatId = msg.chat.id;
-  const text = msg.text;
-
+  text = msg.text;
+  
   // Reply text to user to choose one option
   const reply = `*Recived your text:* \n${text} \n\nChoose one of the options:`;
 
   // Sending reply with inline options for what to do
   bot.sendMessage(chatId, reply, getReplyOptions(text));
+});
 
-  // Listening for user callback of option he clicked
-  bot.on("callback_query", async (query) => {
-    // Query data
-    const type = query.data;
-    const msgId = query.message.message_id;
+// Listening for user callback of option he clicked
+bot.on("callback_query", async (query) => {
+  // Query data
+  const chatId = query.from.id;
+  const type = query.data;
+  const msgId = query.message.message_id;
+  
+  // Answering the query according to query option he chose
+  await bot.answerCallbackQuery(query.id);
+  
+  // If the option is 'text' or 'redirect'
+  if (type === "text" || type === "redirect") {
+    // Getting document and its id to save data
+    const doc = getDocRef();
+    const docId = doc.id;
 
-    // Answering the query according to query option he chose
-    await bot.answerCallbackQuery(query.id);
+    // Generating a random key to encrypt
+    const key = genKey();
 
-    // If the option is 'text' or 'redirect'
-    if (type === "text" || type === "redirect") {
-      // Getting document and its id to save data
-      const doc = getDocRef();
-      const docId = doc.id;
+    // Link of the secret
+    const link = `Your secret link:\nhttps://st.hrvs.me/${docId}#${key}`;
 
-      // Generating a random key to encrypt
-      const key = genKey();
+    // Editing the original message to avoid clutter and duplicate clicking
+    bot.editMessageText(link, getEditMsgOptions(chatId, msgId));
 
-      // Link of the secret
-      const link = `Your secret link:\nhttps://st.hrvs.me/${docId}#${key}`;
+    // Saving the secret to database
+    await saveSecret(getData(text, key, type), doc);
+    bot.sendMessage(chatId, "Saved! 👍🏼");
+  }
 
-      // Editing the original message to avoid clutter and duplicate clicking
-      bot.editMessageText(link, getEditMsgOptions(chatId, msgId));
+  // If the option is 'decrypt'
+  if (type === "decrypt") {
+    try {
+      // Getting id and hash from link
+      const { id, hash } = getIdandHash(text);
 
-      // Saving the secret to database
-      await saveSecret(getData(text, key, type), doc);
-      return;
-    }
+      // Getting secret with id
+      const res = await getSecret(id);
 
-    // If the option is 'decrypt'
-    if (type === "decrypt") {
-      try {
-        // Getting id and hash from link
-        const { id, hash } = getIdandHash(text);
+      // Checking if document exists without any error
+      if (res.success && res.data) {
+        // Decrypting data by hash
+        const data = decrypt(res.data.data, hash);
 
-        // Getting secret with id
-        const res = await getSecret(id);
-
-        // Checking if document exists without any error
-        if (res.success && res.data) {
-          // Decrypting data by hash
-          const data = decrypt(res.data.data, hash);
-
-          // Checking if decryption is successful
-          if (data) {
-            const secret = data.secret;
-            bot.editMessageText(secret, getEditMsgOptions(chatId, msgId));
-          } else {
-            const text = "Your key is invalid";
-            bot.editMessageText(text, getEditMsgOptions(chatId, msgId));
-          }
+        // Checking if decryption is successful
+        if (data) {
+          const secret = data.secret;
+          bot.editMessageText(secret, getEditMsgOptions(chatId, msgId));
         } else {
-          const text = "Secret has been revealed before!";
+          const text = "Your key is invalid";
           bot.editMessageText(text, getEditMsgOptions(chatId, msgId));
         }
-      } catch (error) {
-        console.log(error);
-        const text =
-          "Something terrible happended on our end! \n *Status code:* 500";
+      } else {
+        const text = "Secret has been revealed before!";
         bot.editMessageText(text, getEditMsgOptions(chatId, msgId));
       }
+    } catch (error) {
+      console.log(error);
+      const text =
+        "Something terrible happended on our end! \n *Status code:* 500";
+      bot.editMessageText(text, getEditMsgOptions(chatId, msgId));
     }
-  });
+  }
 });
 
 const getEditMsgOptions = (chatId, messageId) => {
