ae1efaa863152d0a636d238ef7c7e96900b2e9c5
kingvyas <kumarvyasss@gmail.com>
2022-09-14 13:50:13 +0530

    added welcome and help message

--- index.js
+++ index.js
@@ -13,21 +13,31 @@ let text;
 bot.on("message", async (msg) => {
   // Chat id and text sent by user
   const chatId = msg.chat.id;
+  const name = msg.from.first_name;
   text = msg.text;
+
+  // Sending welcome and help message on specific command
+  if (text === "/start" || text === "/help") {
+    await bot.sendMessage(chatId, getWelcomeMsg(name), {
+      parse_mode: "Markdown",
+      disable_web_page_preview: true,
+    });
+    return;
+  }
 
   // Checking if text is a URL
   const httpText = addHttp(text);
   const isTextURL = isUrl(httpText);
 
   // Changing text if it is a URL
-  text = isTextURL ? httpText : text
+  text = isTextURL ? httpText : text;
 
   // Showing options only if text is URL
   if (isTextURL) {
-    const reply = "*Recived your text 📥:* \nChoose one of the options :";
+    const reply = "*Recived your text:* \nChoose one of the options :";
     bot.sendMessage(chatId, reply, getReplyOptions(isTextURL));
   } else {
-    const reply = "Sending you your secret 📩";
+    const reply = "Sending you your secret";
     const m = await bot.sendMessage(chatId, reply, getReplyOptions(isTextURL));
     sendMessage(text, chatId, m.message_id);
   }
@@ -56,23 +66,13 @@ const sendMessage = async (text, chatId, msgId, type = "text") => {
   const key = genKey();
 
   // Link of the secret
-  const link = `Your one-time secret link:\nhttps://st.hrvs.me/${docId}#${key}`;
+  const link = `Your one-time secret link: \n\n*https://st.hrvs.me/${docId}#${key}*`;
 
   // Editing the original message to avoid clutter and duplicate clicking
   bot.editMessageText(link, getEditMsgOptions(chatId, msgId));
 
   // Saving the secret to database
   await saveSecret(getData(text, key, type), doc);
-  bot.sendMessage(chatId, "Saved! 👍🏼");
-};
-
-const getEditMsgOptions = (chatId, messageId) => {
-  return {
-    chat_id: chatId,
-    message_id: messageId,
-    parse_mode: "Markdown",
-    disable_web_page_preview: true,
-  };
 };
 
 const getData = (secret, key, type = "text") => {
@@ -83,6 +83,15 @@ const getData = (secret, key, type = "text") => {
     readReceiptEmail: "",
   };
   return encrypt(data, key);
+};
+
+const getEditMsgOptions = (chatId, messageId) => {
+  return {
+    chat_id: chatId,
+    message_id: messageId,
+    parse_mode: "Markdown",
+    disable_web_page_preview: true,
+  };
 };
 
 const getReplyOptions = (isTextURL) => {
@@ -97,4 +106,8 @@ const getReplyOptions = (isTextURL) => {
   };
 
   return isTextURL ? { parse_mode, reply_markup } : { parse_mode };
+};
+
+const getWelcomeMsg = (name = "user") => {
+  return `Hello ${name} 👋🏼,\n\nSend me a secret and I will return with a *one-time* secret link. \nThe secret can be anything which is confidential like *password, key token, card number* or *OTP*.\n\nYou can also send me *URL* and I will ask if you want to redirect to it. \n\nYour data will always be *end-to-end encrypted* with the *HASH* which is never saved. \n\nFor more info: [Open Secretly](https://st.hrvs.me)`;
 };
