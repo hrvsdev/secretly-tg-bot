28dfea860d54263c0f4a895ec08623d8cf309523
kingvyas <kumarvyasss@gmail.com>
2022-09-16 05:54:33 +0530

    removed unnecessary code

--- mod.ts
+++ mod.ts
@@ -1,6 +1,6 @@
 import { serve } from "https://deno.land/std@0.154.0/http/server.ts";
 import { webhookCallback } from "https://deno.land/x/grammy@v1.11.0/mod.ts";
-// You might modify this to the correct way to import your `Bot` object.
+
 import bot from "./bot.ts";
 
 const handleUpdate = webhookCallback(bot, "std/http");
